85f59eff5001eddafe55fe674bf4ee19a4c2035a
W.Lee <rnsk8945@gmail.com>
2021-06-11 00:25:54 +0900

    [bugfix]main_init

--- main/main_triple_screen.py
+++ main/main_triple_screen.py
@@ -32,12 +32,13 @@ from Utility import Tools as tool
 if __name__ == '__main__':
     try:
         #  7일간의 데이터 개수가 일정개수 이상인 주식만 자동매매 대상으로 할 것
+        # =======================================
+        sg.init_global()
+        # =======================================
         today = datetime.today().weekday()
         if today == 5 or today == 6:  # 토요일이나 일요일이면 자동 종료
             sg.g_logger.write_log(f"本日は土日または日曜なので株取引プログラムを終了します。", log_lv=2, is_slacker=True)
             sys.exit(0)
-        # =======================================
-        sg.init_global()
         # =======================================
         if sg.g_creon_login.check_login_creon() is False:
             sg.g_creon_login.LoginCreon()
