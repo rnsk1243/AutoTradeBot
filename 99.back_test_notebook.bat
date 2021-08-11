76652fbe49993112190cb62bddc206218d3aebbd
whLee <rnsk8945@gmail.com>
2021-08-11 19:24:25 +0900

    배치파일 버전관리대상 제외

--- 99.back_test_notebook.bat
+++
@@ -1,3 +0,0 @@
-color a
-cd c:\stockauto\main
-python c:\stockauto\main\main_back_test.py 20 100 100 62 69 360
--- back_test_no_slow_d.bat
+++
@@ -1,3 +0,0 @@
-color a
-cd Z:\stockauto\main
-python Z:\stockauto\main\main_back_test.py 2 61 61
